4cdb2a89635c5ed89c78e1ee23a5ae1a568f2342
Anish Moorthy <anlsh@protonmail.com>
2020-04-29 13:27:56 -0400

    Renaming

--- main.lisp
+++ main.lisp
@@ -3,13 +3,13 @@
 (defun ncols (im) (second (array-dimensions im)))
 (defun nrows (im) (first (array-dimensions im)))
 
-(defun enumerate-range (start end)
+(defun range (start end)
   ;; Returns a list [start, end>
   (if (>= start end)
       nil
-      (cons start (enumerate-range (1+ start) end))))
+      (cons start (range (1+ start) end))))
 
-(defun combinations (seq1 seq2)
+(defun product (seq1 seq2)
   (reduce #'append (mapcar (lambda (e2) (mapcar (lambda (e1) (list e1 e2)) seq1)) seq2)))
 
 (defun 2d-window (arr row col width height)
@@ -17,7 +17,7 @@
     (mapcar (lambda (offsets)
               (destructuring-bind (srow scol) offsets
                 (setf (numcl:aref slice srow scol) (numcl:aref arr (+ row srow) (+ col scol)))))
-            (combinations (enumerate-range 0 height) (enumerate-range 0 width)))
+            (product (range 0 height) (range 0 width)))
     slice))
 
 (defun make-slices (image filter-ncols filter-nrows)
@@ -28,8 +28,8 @@
                                         filter-ncols filter-nrows)
                              slices)
                     t))
-            (combinations (enumerate-range 0 (1+ (- (nrows image) filter-nrows)))
-                          (enumerate-range 0 (1+ (- (ncols image) filter-ncols)))))
+            (product (range 0 (1+ (- (nrows image) filter-nrows)))
+                     (range 0 (1+ (- (ncols image) filter-ncols)))))
     (alexandria:hash-table-keys slices)))
 
 (defun allowable-offsets (root-img filter)
@@ -43,12 +43,12 @@
                   (destructuring-bind (row col) root-idxs
                     (equalp (numcl:aref root-img row col)
                             (numcl:aref filter (- row row-off) (- col col-off)))))
-                (combinations (enumerate-range (max 0 row-off)
-                                               (min (nrows root-img) (+ filter-nrows row-off)))
-                              (enumerate-range (max 0 col-off)
-                                               (min (ncols root-img) (+ filter-ncols col-off)))))))
-     (combinations (enumerate-range (- 1 (nrows root-img)) (nrows root-img))
-                   (enumerate-range (- 1 (ncols root-img)) (ncols root-img))))))
+                (product (range (max 0 row-off)
+                                (min (nrows root-img) (+ filter-nrows row-off)))
+                         (range (max 0 col-off)
+                                (min (ncols root-img) (+ filter-ncols col-off)))))))
+     (product (range (- 1 (nrows root-img)) (nrows root-img))
+              (range (- 1 (ncols root-img)) (ncols root-img))))))
 
 ;; (defun wave-function-collapse (image filter-ncols filter-nrows output-width output-height)
 ;;   (let ((slices (make-slices image filter-ncols filter-nrows)))))
