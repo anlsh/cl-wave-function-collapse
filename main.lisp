7bc71129075c1782ffbfcbe5eadb0c6346191442
Anish Moorthy <anlsh@protonmail.com>
2020-04-28 21:40:14 -0400

    Write the make-slices function

--- main.lisp
+++ main.lisp
@@ -1,13 +1,36 @@
 (in-package :wfc-cl)
 
-(defun 2d-window (arr row col height width)
+(defun im-width (im) (second (array-dimensions im)))
+(defun im-height (im) (first (array-dimensions im)))
+
+(defun enumerate-range (start end)
+  ;; Returns a list [start, end>
+  (if (>= start end)
+      nil
+      (cons start (enumerate-range (1+ start) end))))
+
+(defun combinations (seq1 seq2)
+  (reduce #'append (mapcar (lambda (e2) (mapcar (lambda (e1) (list e1 e2)) seq1)) seq2)))
+
+(defun 2d-window (arr row col width height)
   (let* ((slice (numcl:zeros (list height width) :type 'integer)))
-    (loop for srow from 0 to (1- height)
-          do (loop for el across (subseq (numcl:aref arr (+ row srow)) col (+ col width))
-                   for scol from 0
-                   do (setf (numcl:aref slice srow scol) el)))
+    (mapcar (lambda (offsets)
+              (destructuring-bind (srow scol) offsets
+                (setf (numcl:aref slice srow scol) (numcl:aref arr (+ row srow) (+ col scol)))))
+            (combinations (enumerate-range 0 height) (enumerate-range 0 width)))
     slice))
 
-;; (defun wave-function-collapse (image filter-width filter-height output-width output-height)
-;;   (destructuring-bind (image-height image-width) (array-dimensions image)
-;;     ))
+(defun make-slices (image filter-width filter-height)
+  ;; Just collect the set of (filter-width, filter-height slices present in image)
+  (let ((slices (make-hash-table :test #'numcl:equalp)))
+    (mapcar (lambda (offs)
+              (setf (gethash (2d-window image (first offs) (second offs)
+                                        filter-width filter-height)
+                             slices)
+                    t))
+            (combinations (enumerate-range 0 (1+ (- (im-height image) filter-height)))
+                          (enumerate-range 0 (1+ (- (im-width image) filter-width)))))
+    (alexandria:hash-table-keys slices)))
+
+(defun wave-function-collapse (image filter-width filter-height output-width output-height)
+  (let ((slices (make-slices image filter-width filter-height)))))
